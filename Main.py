e01bfae4c871f0f06998ed36b2be51c92ec955f8
pranavcoding2010 <110837756+pranavcoding2010@users.noreply.github.com>
2022-08-08 20:18:40 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,9 @@
+num=int(input('Enter a number;')) 
+if (num%5==0 and num%3==0):
+    print("Fizz Buzz")
+elif (num%5==0):
+    print("Fizz")
+elif (num%3==0):
+    print("Buzz")
+else :
+    print(num)
